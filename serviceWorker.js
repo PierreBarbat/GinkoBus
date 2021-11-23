21ad7752545a134c888a81e197a61d147a897bf3
pbarbat3 <pierre.barbat02@edu.univ-fcomte.fr>
2021-11-23 11:26:53 +0100

    serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,4 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
-    e.waitUntil((async () => {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
-  });
+
 
 const cacheName = 'GinkoBusPWA-v1';
 const appShellFiles = [
@@ -13,7 +6,6 @@ const appShellFiles = [
   './index.html',
   './app.js',
   './style.css',
-  './manifest.json',
   './icons/favicon.ico',
   './icons/icon-32.png',
   './icons/icon-64.png',
@@ -24,6 +16,15 @@ const appShellFiles = [
   './icons/icon-256.png',
   './icons/icon-512.png'
 ];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+  });
 
 self.addEventListener('fetch', (e) => {
     e.respondWith((async () => {
